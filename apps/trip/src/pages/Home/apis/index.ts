1822340fd5fbabcac8f78c05df244a47d5fb412a
aoda <aoda2826@qq.com>
2025-04-29 21:10:01 +0800

    Refactor trip APIs and add new "addTrip" function

--- apps/trip/src/pages/Home/apis/index.ts
+++ apps/trip/src/pages/Home/apis/index.ts
@@ -1,6 +1,7 @@
 import httpService from "@/shared/cores/http";
 export const HomeQueryKes = {
   GET_DEFAULT_TRIP_VIEW: "GET_DEFAULT_TRIP_VIEW",
+  ADD_TRIP: "ADD_TRIP",
 };
 type TripViewType = {
   img: string;
@@ -11,4 +12,7 @@ type TripViewType = {
 const getDefaultTripView = () => {
   return httpService.get<TripViewType[]>("document/v1/default-trip-views");
 };
-export { getDefaultTripView };
+const addTrip = () => {
+  return httpService.post("trip/v1/addTrip", { destination: "German", date: "2025-10-01", note: "Zhr shone Staht!" });
+};
+export { getDefaultTripView, addTrip };
